9d0afd40389dcd0a1a607d6f2faab3288d15bfa3
bright <brightigwe417@gmail.com>
2025-05-06 14:12:13 +0100

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,25 +4,25 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer-name">
-        IGWE BRIGHT SUNDAY
+        IGWE BRIGHT SUNDAY 
       </div>
       <div className="footer-links">
         <a href="mailto:brightigwe417@gmail.com" className="footer-icon">
-          <i className="far fa-envelope"></i>
+            <i class="bi bi-envelope-fill"></i>
         </a>
         <a href="https://www.linkedin.com/in/bright-igwe-a705ba361" target="_blank" rel="noopener noreferrer" className="footer-icon">
-          <i className="fab fa-linkedin"></i>
+            <i class="bi bi-linkedin"></i>
         </a>
         <a href="tel:+2348088179399" className="footer-icon">
-          <i className="fas fa-phone"></i>
+            <i class="bi bi-telephone-fill"></i>
         </a>
         <a href="https://github.com/br-1-ght" className="footer-icon">
-          <i className="fab fa-github"></i>
+            <i class="bi bi-github"></i>
         </a>
       </div>
       <div className="footer-copyright">
         <p>IGWE BRIGHT SUNDAY</p>
-        <p className="report-page">Report page</p>
+        <p className="report-page">Report page<i class="bi bi-flag-fill"></i></p>
       </div>
     </footer>
   );
